fbe9eabfad3952f7f7d8d4d88744a3409c238c8b
muthu_29 <muthu310129@gmail.com>
2025-07-28 20:49:04 +0530

    commit

--- src/components/chatbot.jsx
+++ src/components/chatbot.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import "./Chatbot.css";
+import "../components/Chatbot.css";
 
 const Chatbot = () => {
   const [input, setInput] = useState("");
